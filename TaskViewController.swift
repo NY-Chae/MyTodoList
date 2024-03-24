71a74d411f5a8be13822d76073199333e78e66ff
Nayeon Chae <155615768+NY-Chae@users.noreply.github.com>
2024-03-24 22:24:28 +0900

    Add files via upload

---
+++ TaskViewController.swift
@@ -0,0 +1,31 @@
+//
+//  TaskViewController.swift
+//  MyTodoList
+//
+//  Created by 채나연 on 3/20/24.
+//
+
+import UIKit
+
+class TaskViewController: UIViewController {
+    
+    @IBOutlet var label: UILabel!
+    
+    var task: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        label.text = task
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Delete", style:.done, target:self, action: #selector(deleteTask))
+    }
+    
+    @objc func deleteTask() {
+        
+//        let newCount = count - 1
+//      UserDefaults().setValue(newCount, forKey: "count")
+//        UserDefaults().setValue(nil, forKey: "task_\(currentPosition)")
+        
+    }
+}
